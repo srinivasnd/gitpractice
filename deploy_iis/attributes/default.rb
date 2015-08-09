1cbbdac5c855830722d9080c586331d969926bfc
srinivasnd <srinivasnd@live.in>
2015-08-09 14:32:15 +0530

    First commit

---
+++ deploy_iis/attributes/default.rb
@@ -0,0 +1,2 @@
+default['deploy_iis']['docroot'] = 'C:\inetpub\wwwroot'
+default['deploy_iis']['applink'] = 'http://10.0.1.97/DemoWebSite1.zip'
---
+++ deploy_iis/metadata.rb
@@ -0,0 +1,8 @@
+name             'deploy_iis'
+maintainer       'Relevance Lab Pvt Ltd'
+maintainer_email 'devops@relevancelab.com'
+license          'All rights reserved'
+description      'Installs/Configures deploy_iis'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends  "iis"
